4f5b6e38dda243519cb8845191296cdd1f18e231
Riya Vij <riyaviz@benchmarkit.solutions>
2021-08-17 22:53:09 +0530

    Second commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var a = 30;
+var b = 70;
+console.log("The Value of a + b is: ", a + b);
+console.log("The Value of a - b is: ", a - b);
+console.log("The Value of a * b is: ", a * b);
+console.log("The Value of a / b is: ", a / b);
